eb2612ade3fe21d13e83f9e690f0c7d1bbb4ccb4
Miguel Sanz <misanzex@gmail.com>
2025-02-18 13:28:01 +0000

    Creating clean models

---
+++ models/staging/raw/stg_raw__product.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'product') }}
+
+),
+
+renamed as (
+
+    select
+
+
+products_id
+, CAST(purchSE_PRICE AS FLOAT64) AS purchase_price
+
+
+    from source
+
+)
+
+select * from renamed
